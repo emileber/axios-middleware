afe9b79a5c1e3bb37f7377cb7cca4ecdbe913a01
Emile Bergeron <ber.emile@prismalstudio.com>
2018-06-15 16:19:10 -0400

    Removes interceptor completely in favor of promise ready adaptor

--- src/service.js
+++ src/service.js
@@ -20,20 +20,9 @@ export default class HttpMiddlewareService {
         this.unsetHttp();
 
         if (axios) {
-            const interceptors = axios.interceptors;
-
             this.http = axios;
             this.originalAdapter = axios.defaults.adapter;
             axios.defaults.adapter = config => this.adapter(config);
-
-            this._requestInterceptor = interceptors.request.use(
-                config => this._onRequest(config),
-                error => this._onRequestError(error)
-            );
-            this._responseInterceptor = interceptors.response.use(
-                response => this._onResponse(response),
-                error => this._onResponseError(error)
-            );
         }
         return this;
     }
@@ -43,12 +32,7 @@ export default class HttpMiddlewareService {
      */
     unsetHttp() {
         if (this.http) {
-            const interceptors = this.http.interceptors;
-            interceptors.request.eject(this._requestInterceptor);
-            interceptors.response.eject(this._responseInterceptor);
-
             this.http.defaults.adapter = this.originalAdapter;
-
             this.http = null;
         }
         return this;
@@ -108,7 +92,28 @@ export default class HttpMiddlewareService {
      * @returns {Promise}
      */
     adapter(config) {
-        return this._onSync(this.originalAdapter.call(this.http, config));
+        const chain = [conf => this._onSync(this.originalAdapter.call(this.http, conf)), undefined];
+        let promise = Promise.resolve(config);
+
+        this.middlewares.forEach((middleware) => {
+            chain.unshift(
+                middleware.onRequest && (conf => middleware.onRequest(conf)),
+                middleware.onRequestError && (error => middleware.onRequestError(error))
+            );
+        });
+
+        this.middlewares.forEach((middleware) => {
+            chain.push(
+                middleware.onResponse && (response => middleware.onResponse(response)),
+                middleware.onResponseError && (error => middleware.onResponseError(error))
+            );
+        });
+
+        while (chain.length) {
+            promise = promise.then(chain.shift(), chain.shift());
+        }
+
+        return promise;
     }
 
     /**
@@ -121,53 +126,5 @@ export default class HttpMiddlewareService {
             (acc, middleware) => (middleware.onSync ? middleware.onSync(acc) : acc),
             promise
         );
-    }
-
-    /**
-     * @param {Object} config
-     * @returns {Object}
-     * @private
-     */
-    _onRequest(config) {
-        return this.middlewares.reduce(
-            (acc, middleware) => (middleware.onRequest ? middleware.onRequest(acc) : acc),
-            config
-        );
-    }
-
-    /**
-     * @param {Object} error
-     * @returns {Promise<never>}
-     * @private
-     */
-    _onRequestError(error) {
-        this.middlewares.forEach(middleware => middleware.onRequestError &&
-            middleware.onRequestError(error));
-        return Promise.reject(error);
-    }
-
-    /**
-     * @param {Object} response
-     * @returns {*|Object}
-     * @private
-     */
-    _onResponse(response) {
-        return this.middlewares.reduceRight(
-            (acc, middleware) => (middleware.onResponse ? middleware.onResponse(acc) : acc),
-            response
-        );
-    }
-
-    /**
-     * @param {Object} error
-     * @returns {Promise<never>}
-     * @private
-     */
-    _onResponseError(error) {
-        for (let i = this.middlewares.length; i--;) {
-            const middleware = this.middlewares[i];
-            if (middleware.onResponseError) middleware.onResponseError(error);
-        }
-        return Promise.reject(error);
     }
 }
